8e83620960a617da26dc446494fda2cb0ed47db3
Stuart Terrett <shterrett@gmail.com>
2017-05-26 15:20:25 -0400

    Initial Commit

---
+++ lib/binder.ex
@@ -0,0 +1,18 @@
+defmodule Binder do
+  @moduledoc """
+  Documentation for Binder.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Binder.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/binder_test.exs
@@ -0,0 +1,8 @@
+defmodule BinderTest do
+  use ExUnit.Case
+  doctest Binder
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
